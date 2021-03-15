56f4c45b6ceb1d774421a079094e706b6e8f8a8c
Noam Rubabshi <noamr93@gmail.com>
2021-03-15 12:49:44 +0200

    modify

--- get_contour.py
+++ get_contour.py
@@ -10,19 +10,17 @@ def read_curve_data(ds):
     while (0x5000 + (k * 2), 0x3000) in ds:
         curve_data_string = ds[0x5000 + (k * 2), 0x3000].value
         curve_data = np.array(array.array('f', curve_data_string))
-        roi = curve_data
-        x_data = roi[0::2]
-        y_data = roi[1::2]
+        x_data = curve_data[0::2]
+        y_data = curve_data[1::2]
         k += 1
 
-    return x_data, y_data
+    return [x_data, y_data]
 
 
 if __name__ == "__main__":
     ds1 = dcmread("P3/keyframe.dcm")
     ds2 = dcmread("P3/t2.dcm")
-    x, y = read_curve_data(ds1)
-    contour = [x, y]
+    contour = read_curve_data(ds1)
     # print(contour)
     image = ds2.pixel_array
     plt.imshow(image, cmap=plt.cm.gray)
