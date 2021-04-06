26742b3c9a844ea1ce93a7f19f8b6750d3915b57
Noam Rubabshi <noamr93@gmail.com>
2021-04-06 12:33:18 +0300

    modify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import scipy.io
 
 from read_data import read_and_cut_MRI_and_contour
 from Registration import registration
@@ -8,24 +8,26 @@ from calculate_sum_of_intensities import (calculate_sum_of_intensities_inside_st
                                           calculate_sum_of_intensities_inside_mask)
 from LinearRegression import LinearRegression_func
 
-
 if __name__ == "__main__":
     # read patients list
     # patlist = scipy.io.loadmat('patlist_check.mat')
     # print(patlist['mri_data'])
 
     # Open the image files
-    images, contour = read_and_cut_MRI_and_contour(cancerArea=True)
+    images, contour, number_of_images = read_and_cut_MRI_and_contour(patient="P2", cancerArea=True)
+    # print(contour)
 
+    # """
     # registration
     registrate_images = []
     for i in range(len(images)):
         if i == 1:  # the reference image
             registrate_images.append(images[i])
         else:
-            registrate_image = (registration(images[i], images[1], ransacReprojThreshold=3, debug=False))
+            registrate_image = (registration(images[i], images[1], contour, ransacReprojThreshold=4, debug=False))
             registrate_images.append(registrate_image)
-    
+    # """
+
     """
     # check intensity decrease after registration
     x = sum(sum(images[3][10:120,10:120]))
@@ -35,9 +37,12 @@ if __name__ == "__main__":
     """
 
     # calculate sum of pixels intensities in strip, or in the contour by pass 0 in strip_thickness
+    strip_thickness = 9
     intensities_sums = []
-    for i in range(len(registrate_images)):
-        intensities_sums.append(calculate_sum_of_intensities_inside_strip(contour, registrate_images[i], i+1, strip_thickness=9))
+    for i in range(len(images)):
+        intensities_sums.append(calculate_sum_of_intensities_inside_strip(contour, images[i], i + 1,
+                                                                          strip_thickness=strip_thickness))
+    # print(intensities_sums)
 
     # calculate changes in intensity between images
     intensities_changes = []
@@ -46,12 +51,14 @@ if __name__ == "__main__":
         # print("change in intensity between image 1 to image " + str(i+1) + ": " + str(intensities_sums[i] - intensities_sums[0]))
 
     # LinearRegression
-    LinearRegression_func(np.array([2, 3, 4, 5]), intensities_changes)
+    coef, score = LinearRegression_func(np.arange(2, number_of_images), intensities_changes)
+    # LinearRegression_func(np.arange(2, number_of_images), np.log(intensities_changes))  # Linear Regression to ln of res
 
     # show plot of the changes in intensity between images
-    plt.plot(range(2, 6), intensities_changes, color='r')
-    plt.xticks(range(2, 6))  # set the x-labels to integers
-    plt.title("increasing of intensity per pixel compared to the first image.\narea with cancer")
+    plt.plot(range(2, number_of_images), intensities_changes, 'ro')
+    plt.xticks(range(2, number_of_images))  # set the x-labels to integers
+    plt.title("increasing of intensity per pixel compared to the first image.\narea with cancer, inside strip of " +
+              str(strip_thickness)+" pixels.\npressure estimate: "+str(round(coef, 3))+", success rates: "+str(round(score, 3)*100)+"%")
     plt.xlabel("image number")
     plt.ylabel("intensity increase")
     plt.show()
