4de7bc130b7bb2833cffe791847437483a14ec0f
Noam Rubabshi <noamr93@gmail.com>
2021-04-28 15:47:18 +0300

    the main of the project - run the algorithm for all the patients and for all sizes of strips

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from algorithm import algorithm
 
 if __name__ == "__main__":
     # read patients list
-    patlist = scipy.io.loadmat('patlist_check.mat')
-    print(patlist['mri_data'])
-    """
-    for dir in os.listdir("patient"):
-        patient_data = []
-        patient_data.append(dir)
+    # patlist = scipy.io.loadmat('patlist_check.mat')
+    # print(patlist['mri_data'])
 
+    for dir in os.listdir("patient"):
 
         algorithm(patient=dir, strip_thickness=0)  # inside tumor
         for strip in range(3, 10, 2):
             algorithm(patient=dir, strip_thickness=strip)
-            """
