0d5cb2dfb9567cca8863a4d815c59baebe86dfbb
Noam Rubabshi <noamr93@gmail.com>
2021-05-02 01:55:29 +0300

    cancel the append of ones column

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,13 +4,12 @@ import numpy as np
 
 def LinearRegression_func(X, y):
     X = X.reshape(-1, 1)
-    # X = np.concatenate((np.array([[1]] * len(X)), X), axis=1)
-    print("Linear Regression:")
-    print("X:\n" + str(X))
-    print("y: " + str(y))
+    # print("Linear Regression:")
+    # print("X:\n" + str(X))
+    # print("y: " + str(y))
     reg = LinearRegression()
     reg.fit(X, y)
-    print("score: " + str(reg.score(X, y)))
-    print("thetas: " + str(reg.coef_))
+    # print("score: " + str(reg.score(X, y)))
+    # print("thetas: " + str(reg.coef_))
     # print("intercept: " + str(reg.intercept_))
     return reg.coef_[0], reg.score(X, y)
