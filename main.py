9e937c952b091a1a880adb153ef9766cb21645bc
Noam Rubabshi <noamr93@gmail.com>
2021-05-18 21:04:57 +0300

    add: 1. option to cancel show images. 2. run on patients according patient number. 3. in the second analysis add calculations for all kinds of strips (3,5,7,9).

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     """run algorithm for all patients and save the results in patient_res file"""
     """
-    # open patient_res file  for writing
+    # open patient_res file for writing
     patient_res_wb = load_workbook("patient_res.xlsx")
     patient_res_write = patient_res_wb["Data"]
     row = 2
@@ -22,23 +22,23 @@ if __name__ == "__main__":
     patient_info = pd.read_excel("patient_info.xlsx", sheet_name="Data", index_col="patient")
     # print(patient_info)
 
-    for dir in os.listdir("patient"):
+    for dir in sorted(os.listdir("patient"), key=lambda x: int(x.split('P')[1])):
         patient_data = [dir, patient_info.at[dir, 'pathological response'],
                         patient_info.at[dir, 'radiological response']]
 
         # incline inside the tumor
-        patient_data.append(algorithm(patient=dir, strip_thickness=0))
+        patient_data.append(algorithm(patient=dir, strip_thickness=0, show_images=False))
 
         # incline inside strips around the tumor
         for strip in range(3, 10, 2):
-            patient_data.append(algorithm(patient=dir, strip_thickness=strip))
+            patient_data.append(algorithm(patient=dir, strip_thickness=strip, show_images=False))
 
         # incline in area without tumor
-        patient_data.append(algorithm(patient=dir, strip_thickness=0, tumorArea=False,
-                                      rows_start=patient_info.at[dir, 'rows start'],
-                                      rows_end=patient_info.at[dir, 'rows end'],
-                                      columns_start=patient_info.at[dir, 'columns start'],
-                                      columns_end=patient_info.at[dir, 'columns end']))
+        patient_data.append(algorithm(patient=dir, strip_thickness=0, show_images=False, tumorArea=False,
+                                      rows_start=int(patient_info.at[dir, 'rows start']),
+                                      rows_end=int(patient_info.at[dir, 'rows end']),
+                                      columns_start=int(patient_info.at[dir, 'columns start']),
+                                      columns_end=int(patient_info.at[dir, 'columns end'])))
 
         print(patient_data)
         for i in range(len(patient_data)):
@@ -47,6 +47,7 @@ if __name__ == "__main__":
 
     patient_res_wb.save("patient_res.xlsx")
     """
+
     """make analysis on the results in patient_res file"""
 
     # open patient_res file for writing
@@ -63,31 +64,31 @@ if __name__ == "__main__":
          3. patients that the radiological response was successful.
          4. patients that the radiological response was unsuccessful.
        and save the results in patient_res file"""
-    """
+
     write_column = 12
 
     for column in range(4, 8):  # run on all strips
-        strip = patient_res.columns[column]
+        strip = patient_res_read.columns[column]
 
         pathological_successful_treatment_inclines = []
         pathological_unsuccessful_treatment_inclines = []
         radiological_successful_treatment_inclines = []
         radiological_unsuccessful_treatment_inclines = []
     
-        for patient in range(len(patient_res)):
-            if patient_res.at[patient, "pathological response"] == 0 or patient_res.at[patient, "pathological response"] == 1:
-                pathological_successful_treatment_inclines.append(patient_res.at[patient, strip])
-            elif patient_res.at[patient, "pathological response"] == 2:
-                pathological_unsuccessful_treatment_inclines.append(patient_res.at[patient, strip])
+        for patient in range(len(patient_res_read)):
+            if patient_res_read.at[patient, "pathological response"] == 0 or patient_res_read.at[patient, "pathological response"] == 1:
+                pathological_successful_treatment_inclines.append(patient_res_read.at[patient, strip])
+            elif patient_res_read.at[patient, "pathological response"] == 2:
+                pathological_unsuccessful_treatment_inclines.append(patient_res_read.at[patient, strip])
     
-            if patient_res.at[patient, "radiological response"] == 0 or patient_res.at[patient, "radiological response"] == 1:
-                radiological_successful_treatment_inclines.append(patient_res.at[patient, strip])
-            elif patient_res.at[patient, "radiological response"] == 2:
-                radiological_unsuccessful_treatment_inclines.append(patient_res.at[patient, strip])
+            if patient_res_read.at[patient, "radiological response"] == 0 or patient_res_read.at[patient, "radiological response"] == 1:
+                radiological_successful_treatment_inclines.append(patient_res_read.at[patient, strip])
+            elif patient_res_read.at[patient, "radiological response"] == 2:
+                radiological_unsuccessful_treatment_inclines.append(patient_res_read.at[patient, strip])
 
         print(strip, ": ")
-        print(pathological_successful_treatment_inclines, pathological_unsuccessful_treatment_inclines)
-        print(radiological_successful_treatment_inclines, radiological_unsuccessful_treatment_inclines)
+        print("pathological: successful-", pathological_successful_treatment_inclines, "unsuccessful-", pathological_unsuccessful_treatment_inclines)
+        print("radiological: successful-", radiological_successful_treatment_inclines,  "unsuccessful-", radiological_unsuccessful_treatment_inclines)
 
         # write in pathological response - successful treatment
         patient_res_write.cell(6, write_column).value = np.mean(pathological_successful_treatment_inclines, axis=0)
@@ -109,25 +110,24 @@ if __name__ == "__main__":
         patient_res_write.cell(7, write_column).value = np.std(radiological_unsuccessful_treatment_inclines, axis=0)
         write_column += 1
 
-    patient_res_wb.save("patient_res.xlsx")
-    """
-
     """Examining whether in cases where there has no leakage from the tumor
     (when the incline in the strip compared to the incline of a non-cancerous area is less than the threshold value)
-    there is a difference in response to treatment between cases that had an increase inside the tumor
+    if there is a difference in response to treatment between cases that had an increase inside the tumor
     and cases that did not.
     and save the results in patient_res file"""
 
-    write_row = 5
     write_column = 30
-    strip = patient_res_read.columns[4]  # strip of 3 pixels
-    leakage_threshold = 2  # the threshold value for leakage from the tumor
-    for patient in range(len(patient_res_read)):
-        if patient_res_read.at[patient, strip] - patient_res_read.at[patient, "incline in non-cancerous area"] < leakage_threshold:
-            patient_res_write.cell(write_row, write_column).value = patient_res_read.at[patient, "patient"]
-            patient_res_write.cell(write_row, write_column + 1).value = str(patient_res_read.at[patient, "incline inside tumor"] > 0)
-            patient_res_write.cell(write_row, write_column + 3).value = patient_res_read.at[patient, "pathological response"]
-            patient_res_write.cell(write_row, write_column + 5).value = patient_res_read.at[patient, "radiological response"]
-            write_row += 1
-
+    for column in range(4, 8):  # run on all strips
+        strip = patient_res_read.columns[column]
+        write_row = 5
+        # strip = patient_res_read.columns[4]  # strip of 3 pixels
+        leakage_threshold = 2  # the threshold value for leakage from the tumor
+        for patient in range(len(patient_res_read)):
+            if patient_res_read.at[patient, strip] - patient_res_read.at[patient, "incline in non-cancerous area"] < leakage_threshold:
+                patient_res_write.cell(write_row, write_column).value = patient_res_read.at[patient, "patient"]
+                patient_res_write.cell(write_row, write_column + 1).value = str(patient_res_read.at[patient, "incline inside tumor"] > 0)
+                patient_res_write.cell(write_row, write_column + 3).value = patient_res_read.at[patient, "pathological response"]
+                patient_res_write.cell(write_row, write_column + 5).value = patient_res_read.at[patient, "radiological response"]
+                write_row += 1
+        write_column += 7
     patient_res_wb.save("patient_res.xlsx")
